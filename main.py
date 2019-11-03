5fceba16d54c3223630a2f7053244aa5c3034d79
Vickey Pradhan <pradhanvickey@gmail.com>
2019-11-03 12:21:43 +0530

    Add files via upload
    made script to accept positional argument from user.

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import threading
+import argparse
 from queue import Queue
 from spider import Spider
 from domain import *
 from general import *
 from time import  time
 
-PROJECT_NAME = 'MrPradhanSpider1'
-HOMEPAGE = 'https://www.codewithharry.com/'
+def parse_argument():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--project_name",help="directory where queue and crawled file will be stored.",required=True)
+    parser.add_argument("--page_url",help="url for which crawling need to be done.",required=True)
+    parser.add_argument("--no_of_threads",help="no. of threads for crawling. Default is 1. ",default=1,type=int)
+    args = parser.parse_args()
+    return args
+
+start_time = time()
+options = parse_argument()
+PROJECT_NAME = options.project_name
+HOMEPAGE = options.page_url
 DOMAIN_NAME = get_domain_name(HOMEPAGE)
+NUMBER_OF_THREADS = options.no_of_threads
 QUEUE_FILE = PROJECT_NAME + '/queue.txt'
 CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
-NUMBER_OF_THREADS = 1
 queue = Queue()
 Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
-
 
 # Create worker threads (will die when main exits)
 def create_workers():
@@ -46,8 +56,8 @@ def crawl():
         print(str(len(queued_links)) + ' links in the queue')
         create_jobs()
 
-start_time = time()
 create_workers()
 crawl()
 end_time = time()
+
 print('Total time => ',str((end_time-start_time)*1000) +"ms")
